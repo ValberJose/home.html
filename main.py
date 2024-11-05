4a015c3ad8370dd97d4a6816599c898dddd24d21
VALBER JOSE COSTA <162817104+ValberJose@users.noreply.github.com>
2024-11-05 11:47:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from email.mime.text import MIMEText
 import logging
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import make_response
-import streamlit as st
+
 
 # Configurar logging para debug
 logging.basicConfig(level=logging.DEBUG)
